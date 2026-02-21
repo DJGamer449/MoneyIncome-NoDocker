90bb4e3a8a003ee4cf5d97072e9f4844aec94efb
DJGamer449 <khangduonghoang79@gmail.com>
2026-02-21 21:24:20 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -43,7 +43,7 @@ kernel_tune() {
   # ==============================
   # PORT CAPACITY (CRITICAL)
   # ==============================
-  sysctl -w net.ipv4.ip_local_port_range="1000 65535" >/dev/null
+  #sysctl -w net.ipv4.ip_local_port_range="1000 65535" >/dev/null
   sysctl -w net.ipv4.tcp_tw_reuse=1 >/dev/null
   sysctl -w net.ipv4.tcp_fin_timeout=5 >/dev/null
   sysctl -w net.ipv4.tcp_max_tw_buckets=5000000 >/dev/null
